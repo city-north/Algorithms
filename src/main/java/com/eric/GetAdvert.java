fc98fd10623bde6e9b684c662a367b48c3140dd0
EricChen <749829987@qq.com>
2017-11-08 19:20:25 +0800

    广告

--- src/main/java/com/eric/GetAdvert.java
+++ src/main/java/com/eric/GetAdvert.java
@@ -10,36 +10,47 @@ public class GetAdvert {
 
 
     public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            test(i,45,3,20);
+        }
+    }
+    public static void test(Integer pageNum,Integer pageSize,int gap,int listSize){
+        //第几页  //每页数据的个数 //每gap条数据插播一套广告
         List<advert> adverts = new ArrayList<advert>();
-        for(int i = 0;i<10;i++){        //10条广告循环
+        for(int i = 1;i<=listSize;i++){        //10条广告循环
             advert advert = new advert();
-            advert.setUrl("第"+i+"条广告");
+            advert.setUrl("第"+(i)+"条广告");
             adverts.add(advert);
         }
-        int pageNum =7;    //第几页
-        int pageSize = 10;  //每页数据的个数
-        int gap =3;        //每gap条数据插播一套广告
         int addSize = pageSize/gap; //每页AddSize条广告
         System.out.println("每页"+addSize+"条广告");
         int startIndex = (pageNum - 1) * addSize;
         int endIndex =  pageNum * addSize;
-        List<advert> showAdd;
-        if(endIndex>adverts.size()){
-            if(startIndex > adverts.size()){
-                startIndex  = startIndex -adverts.size();
-                showAdd = adverts.subList(startIndex,startIndex+addSize);
-
+        List<advert> showAdd =new ArrayList<advert>();
+        int advertSize = adverts.size();
+        if(endIndex>advertSize&&startIndex>advertSize){
+            //开始大于广告数组长度，结束大于广告数组长度
+            endIndex = endIndex%advertSize;
+            startIndex = startIndex%advertSize;
+            if(startIndex > endIndex){  //开始大于结束
+                showAdd = adverts.subList(startIndex,advertSize);
+                showAdd.addAll(adverts.subList(0, endIndex));
             }else {
-                showAdd = adverts.subList(startIndex,adverts.size());
-                endIndex=endIndex-adverts.size();
-                List<advert> adverts1 = adverts.subList(0, endIndex);
-                showAdd.addAll(adverts1);
+                showAdd = adverts.subList(startIndex,endIndex);
             }
+        }else if(!(endIndex>advertSize)&&startIndex>advertSize){
+            startIndex = startIndex%advertSize;
+            showAdd = adverts.subList(startIndex,advertSize);
+            showAdd.addAll(adverts.subList(0, endIndex));
+        }else if(endIndex>advertSize&&!(startIndex>advertSize)){
+            endIndex = endIndex%advertSize;
+            showAdd = adverts.subList(startIndex,advertSize);
+            showAdd.addAll(adverts.subList(0, endIndex));
         }else {
             showAdd = adverts.subList(startIndex,endIndex);
         }
-
         System.out.println("第"+pageNum+"页");
         System.out.println(showAdd);
     }
+
 }
